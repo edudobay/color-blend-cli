12a620da893deb0806ff95df8ea50bc0a79762c0
Eduardo Dobay <edudobay@gmail.com>
2019-01-25 20:31:57 -0200

    Parse hex RGB colors with alpha channel

--- __tests__/parseColor.js
+++ __tests__/parseColor.js
@@ -1,21 +1,34 @@
 const parseColor = require('../src/parser/parseColor')
 
-test.each([
-  ['ffffff', {rgb: [0xff, 0xff, 0xff]}],
-  ['00BC3D', {rgb: [0x00, 0xbc, 0x3d]}],
-  ['000000', {rgb: [0x00, 0x00, 0x00]}],
-  ['3Fb7cE', {rgb: [0x3f, 0xb7, 0xce]}]]
-)(
-  'parses correctly hex color #%s',
-  (color, parsed) => {
-    expect(parseColor(color)).toEqual(parsed)
-  })
+describe('parse hex RGB colors', () => {
+  test.each([
+    ['ffffff', {rgb: [0xff, 0xff, 0xff]}],
+    ['00BC3D', {rgb: [0x00, 0xbc, 0x3d]}],
+    ['000000', {rgb: [0x00, 0x00, 0x00]}],
+    ['3Fb7cE', {rgb: [0x3f, 0xb7, 0xce]}]]
+  )(
+    'parses correctly hex color #%s',
+    (color, parsed) => {
+      expect(parseColor(color)).toEqual(parsed)
+    })
 
-test.each([['zzz'], ['fffc3']])(
-  'parser rejects invalid color %s',
-  (color) => {
-    expect(() => parseColor(color)).toThrow()
-  })
+  test.each([['zzz'], ['fffc3']])(
+    'parser rejects invalid color %s',
+    (color) => {
+      expect(() => parseColor(color)).toThrow()
+    })
+})
+
+describe('parse hex RGBA colors', () => {
+  test.each([
+    ['argb:ffff0000', {rgba: [0xff, 0x00, 0x00, 0xff]}],
+    ['rgba:00ccffcc', {rgba: [0x00, 0xcc, 0xff, 0xcc]}],
+  ])(
+    'parses correctly hex color %s',
+    (color, parsed) => {
+      expect(parseColor(color)).toEqual(parsed)
+    })
+})
 
 test('parser rejects empty color', () => {
   expect(() => parseColor('')).toThrow()
--- src/parser/parseColor.js
+++ src/parser/parseColor.js
@@ -1,5 +1,23 @@
+function parseHexLiteral(numBytes) {
+
+}
+
 function parseColor(color) {
-  if (/[0-9a-fA-F]{6}/.test(color)) {
+  if (color.startsWith('rgba:')) {
+    if (/^rgba:[0-9a-fA-F]{8}$/.test(color)) {
+      const components = [0, 1, 2, 3].map(i => parseInt(color.substr(5 + 2*i, 2), 16))
+      return {rgba: components}
+    }
+  }
+
+  if (color.startsWith('argb:')) {
+    if (/^argb:[0-9a-fA-F]{8}$/.test(color)) {
+      const components = [1, 2, 3, 0].map(i => parseInt(color.substr(5 + 2*i, 2), 16))
+      return {rgba: components}
+    }
+  }
+
+  if (/^[0-9a-fA-F]{6}$/.test(color)) {
     const components = [0, 1, 2].map(i => parseInt(color.substr(2*i, 2), 16))
     return {rgb: components}
   }
