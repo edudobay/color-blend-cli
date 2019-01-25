e96a75c35dfecc7f88d7cfc4a5b9e64076c3e18b
Eduardo Dobay <edudobay@gmail.com>
2019-01-25 20:01:39 -0200

    Parse simple color operations

---
+++ __tests__/parseOperation.js
@@ -0,0 +1,31 @@
+const parseOperation = require('../src/parser/parseOperation')
+
+function args(str) {
+  return str.split(' ')
+}
+
+test('parses correctly \'over\' operation between two hex colors', () => {
+  expect(parseOperation(args('000000 over ffffff'))).toEqual({
+    bg: 'ffffff',
+    fg: '000000'
+  })
+})
+
+test('parses correctly over operation with decimal rgb colors', () => {
+  expect(parseOperation(args('255,255,0 over 0,0,0'))).toEqual({
+    bg: '0,0,0',
+    fg: '255,255,0'
+  })
+})
+
+test('parser rejects incomplete \'over\' operation', () => {
+  expect(() => {
+    parseOperation(args('000000 over'))
+  }).toThrow()
+})
+
+test('parser rejects invalid operation', () => {
+  expect(() => {
+    parseOperation(args('000000 sometimes ffffff'))
+  }).toThrow()
+})
---
+++ src/parser/parseOperation.js
@@ -0,0 +1,17 @@
+function parseOperation(args) {
+  if (args.length !== 3) {
+    throw new Error('invalid argument')
+  }
+
+  const [lhs, operator, rhs] = args
+  if (operator !== 'over') {
+    throw new Error('invalid operator')
+  }
+
+  return {
+    bg: rhs,
+    fg: lhs,
+  }
+}
+
+module.exports = parseOperation
