8684a8a3005d88a2fcd2592b4fd4214405ccd0c6
Eduardo Dobay <edudobay@gmail.com>
2019-01-25 20:43:16 -0200

    Extract hex-literal parsing function

--- src/parser/parseColor.js
+++ src/parser/parseColor.js
@@ -1,28 +1,30 @@
-function parseHexLiteral(numBytes) {
+function range(n) {
+  return [...Array(n).keys()]
+}
 
+function parseHexLiteral(literal, numBytes, order = null) {
+  const expectedLength = numBytes * 2
+  const re = new RegExp(`^[0-9a-fA-F]{${expectedLength}}$`)
+
+  if (!re.test(literal)) {
+    throw new Error(`invalid hex literal for length ${numBytes}`)
+  }
+
+  return (order ? order : range(numBytes)).map(i =>
+    parseInt(literal.substr(2 * i, 2), 16)
+  )
 }
 
 function parseColor(color) {
   if (color.startsWith('rgba:')) {
-    if (/^rgba:[0-9a-fA-F]{8}$/.test(color)) {
-      const components = [0, 1, 2, 3].map(i => parseInt(color.substr(5 + 2*i, 2), 16))
-      return {rgba: components}
-    }
+    return {rgba: parseHexLiteral(color.substr(5), 4)}
   }
 
   if (color.startsWith('argb:')) {
-    if (/^argb:[0-9a-fA-F]{8}$/.test(color)) {
-      const components = [1, 2, 3, 0].map(i => parseInt(color.substr(5 + 2*i, 2), 16))
-      return {rgba: components}
-    }
+    return {rgba: parseHexLiteral(color.substr(5), 4, [1, 2, 3, 0])}
   }
 
-  if (/^[0-9a-fA-F]{6}$/.test(color)) {
-    const components = [0, 1, 2].map(i => parseInt(color.substr(2*i, 2), 16))
-    return {rgb: components}
-  }
-
-  throw new Error('invalid color literal')
+  return {rgb: parseHexLiteral(color, 3)}
 }
 
 module.exports = parseColor
